ce949143c89b7d096d107c6df42bf8a19e29b464
Franz Bernack <franz.bernack@cranky.de>
2024-03-22 16:46:49 +0100

    Optimize

--- content.js
+++ content.js
@@ -15,13 +15,13 @@ const observer = new MutationObserver(() => {
     .filter(commentBox => !commentBox.hasAttribute("data-mutated"))
     .forEach(commentBox => {
       commentBox.setAttribute("data-mutated", "true");
-      addSuggestionButton(commentBox, createPrompt(commentBox));
+      addSuggestionButton(commentBox);
     });
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-const addSuggestionButton = (commentBox, prompt) => {
+const addSuggestionButton = (commentBox) => {
   const button = document.createElement("div");
   button.classList.add(
     "artdeco-button",
@@ -32,7 +32,7 @@ const addSuggestionButton = (commentBox, prompt) => {
   button.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb-fill" viewBox="0 0 16 16"><path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/></svg>';
   button.addEventListener("click", async () => {
-    const suggestion = await fetchSuggestion(prompt)
+    const suggestion = await fetchSuggestion(createPrompt(commentBox));
     commentBox.querySelector(".ql-editor").innerHTML = `<p>${suggestion}</p>`;
   });
   commentBox.querySelector(".mlA").prepend(button);
